ece3ca70a217b6e86f3652434f0d22692d1fb394
Mark McDonnell <mark.mcdx@gmail.com>
2013-08-30 09:43:54 +0100

    Add form posting and file uploading examples

--- Express/routes.js
+++ Express/routes.js
@@ -11,6 +11,11 @@ module.exports = function (app) {
         helpers.loadPage(req, res, 'public/home.html');
     });
 
+    /*
+        Any files that don't exist in our static directory can 
+        still be accessed if we specifically add a route for them
+     */
+    
     app.get('/testing.html', function (req, res) {
         helpers.loadPage(req, res, __dirname + req.route.path);
     });
@@ -19,7 +24,21 @@ module.exports = function (app) {
         if (req.params.name === 'mark') {
             res.end('You were after: ' + req.params.name + ', boom!');
         } else {
-            next(); // let some other middleware take it if they want it (if no other middleware, for example, another `get` call takes it then the page 404's)
+            next(); // let some other middleware take it if they want it (if no other middleware, for example another `get` call, takes over then the page 404's)
         }
+    });
+
+    app.post('/postdata', function (req, res) {
+        res.end('Thanks, your full name is "' + req.body.fullname + '" and you are "' + req.body.age + '" years old');
+    });
+
+    app.post('/uploadfile', function (req, res) {
+        console.log(req.files.fileToUpload);
+
+        // The bodyParser middleware automatically saves the file
+        // So we need to make sure that we're not wasting space by saving the same file
+        // TODO
+        
+        res.end('Thanks for the file');
     });
 };
--- Express/server.js
+++ Express/server.js
@@ -3,7 +3,10 @@ var express = require('express'),
 
 app.use(express.compress())         // gzip all content
    .use(express.static('public'))   // serve static files from this directory
-   .use(express.bodyParser())       // handle file uploads
+   .use(express.bodyParser({ 
+        keepExtensions: true, 
+        uploadDir: __dirname + '/file-uploads' 
+   }))                              // handle file uploads
    .use(express.responseTime());    // send response time via header
 
 require('./routes')(app);
