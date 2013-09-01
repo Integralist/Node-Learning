04f27fed2953e6c8dd537e7677eea55d3b135971
Mark McDonnell <mark.mcdx@gmail.com>
2013-09-01 19:42:26 +0100

    Global error handler

--- Express/server.js
+++ Express/server.js
@@ -7,8 +7,21 @@ app.use(express.compress())         // gzip all content
         keepExtensions: true, 
         uploadDir: __dirname + '/file-uploads' 
    }))                              // handle file uploads
-   .use(express.responseTime());    // send response time via header
+   .use(express.responseTime())     // send response time via header
+   .use(function (err, req, res, next) {
+        res.status(500);
+        res.end(JSON.stringify(err) + '\n');
+   });                              // global error handler
 
 require('./routes')(app);
 
 app.listen(8080);
+
+// Global application-wide error handler
+// Only use for logging purposes (otherwise your app is very unhappy at this point)
+process.on('uncaughtException', function (err) {
+    console.log('Caught exception: ' + err);
+    process.exit(-1); // terminate the node process
+});
+
+console.log(process);
