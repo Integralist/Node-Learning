74711e2f625c6583779bdd5ad11563b4a0bcecb5
Mark McDonnell <mark.mcdx@gmail.com>
2013-09-02 08:59:13 +0100

    Move application settings into a configuration file

--- Express/server.js
+++ Express/server.js
@@ -1,18 +1,7 @@
 var express = require('express'),
     app = express();
 
-app.use(express.compress())         // gzip all content
-   .use(express.static('public'))   // serve static files from this directory
-   .use(express.bodyParser({ 
-        keepExtensions: true, 
-        uploadDir: __dirname + '/file-uploads' 
-   }))                              // handle file uploads
-   .use(express.responseTime())     // send response time via header
-   .use(function (err, req, res, next) {
-        res.status(500);
-        res.end(JSON.stringify(err) + '\n');
-   });                              // global error handler
-
+require('./configuration')(app, express);
 require('./routes')(app);
 
 app.listen(8080);
