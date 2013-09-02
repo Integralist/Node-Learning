51ffaea7e9b13ebcd045afec7689c06551822f5a
Mark McDonnell <mark.mcdx@gmail.com>
2013-09-02 09:00:47 +0100

    Forgot to include the new configuration file

---
+++ Express/configuration.js
@@ -0,0 +1,22 @@
+module.exports = function (app, express) {
+    // gzip all content
+    app.use(express.compress());
+
+    // serve static files from this directory
+    app.use(express.static('public'));
+
+    // handle file uploads
+    app.use(express.bodyParser({ 
+        keepExtensions: true, 
+        uploadDir: __dirname + '/file-uploads' 
+    }));
+
+    // send response time via header
+    app.use(express.responseTime());
+
+    // global error handler
+    app.use(function (err, req, res, next) {
+        res.status(500);
+        res.end(JSON.stringify(err) + '\n');
+    });
+};
